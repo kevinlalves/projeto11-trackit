7edfc603cd9747d2005d14890529b6ce68e63cd8
kevin <kevin.alves.dev@gmail.com>
2022-12-20 10:07:37 -0300

    fix bug where progress wans't updated correctly

--- src/pages/habits/components/Habit.js
+++ src/pages/habits/components/Habit.js
@@ -1,28 +1,32 @@
 import remove from "../../../assets/remove.png";
 import { useContext, useState } from "react";
 import UserContext from "../../../contexts/UserContext";
-import { deleteHabit } from "../../../services/trackit-api";
+import { deleteHabit, listTodaysHabits } from "../../../services/trackit-api";
 import HabitStyle from "../../../style/HabitStyle";
 import Days from "../../../components/Days";
 import ProgressContext from "../../../contexts/ProgressContext";
 import { dialogue } from "../../../constants/i18n-br";
+import { getProgress } from "../../../constants/utils";
 
 export default function Habit({ habit, setHabits, habits }) {
   const { user } = useContext(UserContext);
-  const {progress, setProgress} = useContext(ProgressContext);
+  const { setProgress } = useContext(ProgressContext);
   const [disabled, setDisabled] = useState(false);
   const { id, name, days } = habit;
 
   const removeHabit = async () => {
     setDisabled(true);
     if (disabled || !window.confirm(dialogue.delete)) {
-      return ;
+      return;
     }
 
     await deleteHabit(id, user.token);
-    setProgress(progress - 100/habits.length);
     setHabits(habits.filter(habit => habit.id !== id));
     setDisabled(false);
+    const todayHabits = await listTodaysHabits(user.token);
+    const { progress } = getProgress(todayHabits);
+    setProgress(progress);
+
   };
 
   return (
